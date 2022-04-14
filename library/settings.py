0af577b295b82412e35ec57b1fa591596fe33d5c
AntonyBrowm <antonybrowm@gmail.com>
2022-04-14 03:44:28 +0000

    4 attempt

--- library/settings.py
+++ library/settings.py
@@ -101,9 +101,9 @@ if os.environ.get('GITHUB_WORKFLOW'):
     DATABASES = {
         'default': {
            'ENGINE': 'django.db.backends.postgresql',
-           'NAME': 'jesus_uribedb',
-           'USER': 'jesus_uribe',
-           'PASSWORD': '1701',
+           'NAME': 'antony_juarezdb',
+           'USER': 'antony_juarez',
+           'PASSWORD': 'fassi080904',
            'HOST': 'localhost',
            'PORT': '5432',
         }
